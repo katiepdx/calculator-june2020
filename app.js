d9f2a45a670ab65b5b9095a539d1ed79f6fa61cd
Katie <61799774+katiepdx@users.noreply.github.com>
2020-06-25 13:59:52 -0700

    add subtract ok

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//ADDITION
 // get dom elements
 // get add-input-1
 const addInput1 = document.getElementById('add-input-1');
@@ -24,4 +25,40 @@ boogerButton.addEventListener('click', () => {
 
     // update the textContent of the span
     boogerSpan.textContent = sum;
+});
+
+//SUBTRACTION
+//Dom Elements
+// GET subtract-input-1
+const subtractionInput1 = document.getElementById('subtract-input-1');
+//GET subtract-input-2
+const subtractionInput2 = document.getElementById('subtract-input-2');
+//OK VALIDATE GOT 2 input fields  
+console.log(subtractionInput1, subtractionInput2);
+//GET subtraction-button
+const subtractionButton = document.getElementById('subtract-button');
+//VALIDATE GOT button
+// console.log(subtractionButton);
+//GET subtraction-result
+const subtractionResult = document.getElementById('subtraction-result');
+//VALIDATE GOT subtraction-result
+//console.log(subtractionResult);
+
+//INITIALIZE STATE
+
+//SET the event listener to the button - on a click do something
+subtractionButton.addEventListener('click', () => {
+    // post-click do this
+    // make sure we have the inputs (console.log)
+    // make sure we have the span
+    // make sure we have the .values of the inputs (the value the user inputs) 
+    const subtractionNum1 = subtractionInput1.value;
+    const subtractionNum2 = subtractionInput2.value;
+    console.log(subtractionNum1, subtractionNum2);
+    
+    //Subtract num1 from num2
+    const subtractNumers = Number(subtractionNum1) - Number(subtractionNum2);
+    
+    //Print result to span
+    subtractionResult.textContent = subtractNumers;
 });
